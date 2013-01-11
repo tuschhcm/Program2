ab200b6156f5cae0541ae479f388057b0204c90d
Craig Tuschhoff <tuschhcm@gmail.com>
2013-01-10 17:19:59 -0800

    tabs to spaces

--- DriverExam.java
+++ DriverExam.java
@@ -1,39 +1,39 @@
 import java.util.ArrayList;
 
 public class DriverExam {
-   
+
    //Instance fields
-   private char[] correctAnswers = 
+   private char[] correctAnswers =
    {'B','D','A','A','C','A','B','A','C','D','B','C','D','A','D','C','C','B','D','A'};
-   
+
    private char[] studentAnswers;
-	
-	final private static int TEST_LENGTH = 20;
-   
+
+   final private static int TEST_LENGTH = 20;
+
    //Constructor
    public DriverExam(char[] c) {
-      
+
       studentAnswers = new char[TEST_LENGTH];
-      
+
       //move passed array values to instance field
       for(int i = 0; i < c.length; i++) {
          studentAnswers[i] = c[i];
       }
    }
-   
+
    /**
       the passed method compares studentAnswers to
       correctAnswers and returns true if the
       answers match, false if they do not.
    */
    public boolean passed() {
-      
+
       boolean pass;
       int index = 0;
-      
+
       //compare answers
       do{
-			
+
          if(studentAnswers[index] == correctAnswers[index]) {
             pass = true;
             index++;
@@ -41,115 +41,114 @@ public class DriverExam {
             pass = false;
          }
       }while(pass == true && index < studentAnswers.length);
-      
-      return pass;   
-   }
-   
-	/**
-		the questionsMissed method returns an array of
-		the question numbers where studentAnswers and
-		correctAnswers differed
-	*/
-   public int[] questionsMissed() {
-      
-		//ArrayList used because number of incorrect submissions is unknown
-		ArrayList<Integer> wrongAnswers = new ArrayList<Integer>();
-		
-		int index = 0;	//keep track of question being graded
-		
-		int[] returnAnswers;	//holds array to be returned to caller
-		
-		//loop through answers to find incorrect submissions
-		do{
-			if(studentAnswers[index] == correctAnswers[index]) {
-				index ++;
-			} else {
-				wrongAnswers.add(index + 1);
-				index++;
-			}
-		}while(index < studentAnswers.length);
-  
-  		//move incorrect submissions into a normal array
-  		returnAnswers = new int[wrongAnswers.size()];    
-		
-		for(int i = 0; i < wrongAnswers.size(); i++) {
-			returnAnswers[i] = wrongAnswers.get(i);
-		}
-		
-		//return incorrrect answers
-		return returnAnswers;
-   }
-   
-	/**
-		the totalCorrect method returns the total number
-		of questions the user got right
-	*/
-   public int totalCorrect() {
-      
-		//holds position of current read
-		int index = 0;
-		int rightAnswerCount = 0;
-		
-		//loop through answers and count correct submissions
-		do{
-			if(studentAnswers[index] == correctAnswers[index]) {
-				rightAnswerCount++;
-				index ++;
-			} else {
-				index++;
-			}
-		}while(index < studentAnswers.length);
 
-		
+      return pass;
+   }
+
+   /**
+      the questionsMissed method returns an array of
+      the question numbers where studentAnswers and
+      correctAnswers differed
+   */
+   public int[] questionsMissed() {
+
+      //ArrayList used because number of incorrect submissions is unknown
+      ArrayList<Integer> wrongAnswers = new ArrayList<Integer>();
+
+      int index = 0; //keep track of question being graded
+
+      int[] returnAnswers; //holds array to be returned to caller
+
+      //loop through answers to find incorrect submissions
+      do{
+         if(studentAnswers[index] == correctAnswers[index]) {
+            index ++;
+         } else {
+            wrongAnswers.add(index + 1);
+            index++;
+         }
+      }while(index < studentAnswers.length);
+
+      //move incorrect submissions into a normal array
+      returnAnswers = new int[wrongAnswers.size()];
+
+      for(int i = 0; i < wrongAnswers.size(); i++) {
+         returnAnswers[i] = wrongAnswers.get(i);
+      }
+
+      //return incorrrect answers
+      return returnAnswers;
+   }
+
+   /**
+      the totalCorrect method returns the total number
+      of questions the user got right
+   */
+   public int totalCorrect() {
+
+      //holds position of current read
+      int index = 0;
+      int rightAnswerCount = 0;
+
+      //loop through answers and count correct submissions
+      do{
+         if(studentAnswers[index] == correctAnswers[index]) {
+            rightAnswerCount++;
+            index ++;
+         } else {
+            index++;
+         }
+      }while(index < studentAnswers.length);
+
+
       return rightAnswerCount;
    }
-   
-	/**
-		the totalIncorrect method returns the total number
-		of questions the user got wrong
-	*/
-   public int totalIncorrect() {
-      
-      //holds position of current read
-		int index = 0;
-		int wrongAnswerCount = 0;
-		
-		//loop through answers and count correct submissions
-		do{
-			if(studentAnswers[index] == correctAnswers[index]) {
-				index ++;
-			} else {
-				wrongAnswerCount++;
-				index++;
-			}
-		}while(index < studentAnswers.length);
 
-		
+   /**
+      the totalIncorrect method returns the total number
+      of questions the user got wrong
+   */
+   public int totalIncorrect() {
+
+      //holds position of current read
+      int index = 0;
+      int wrongAnswerCount = 0;
+
+      //loop through answers and count correct submissions
+      do{
+         if(studentAnswers[index] == correctAnswers[index]) {
+            index ++;
+         } else {
+            wrongAnswerCount++;
+            index++;
+         }
+      }while(index < studentAnswers.length);
+
+
       return wrongAnswerCount;
    }
 
    public static void main(String[] args) {
       char[] s = {'B','A','A','A','C','A','B','A','C','D','B','C','D','A','D','C','C','B','D','D'};
       DriverExam e = new DriverExam(s);
-		
-		//test passed method
-		System.out.print("Pass/Fail: ");
-      System.out.println(e.passed());
-		
-		//print questions missed
-		System.out.println("\nQuestions Missed: ");
-		for(int i = 0; i < e.questionsMissed().length; i++) {
-			System.out.println(e.questionsMissed()[i]);
-		}
-		
-		//test totalCorrect
-		System.out.print("\nTotal Correct: ");
-		System.out.print(e.totalCorrect());
-		
-		//test totalCorrect
-		System.out.print("\nTotal Incorrect: ");
-		System.out.print(e.totalIncorrect());
-   }
 
+      //test passed method
+      System.out.print("Pass/Fail: ");
+      System.out.println(e.passed());
+
+      //print questions missed
+      System.out.println("\nQuestions Missed: ");
+      for(int i = 0; i < e.questionsMissed().length; i++) {
+         System.out.println(e.questionsMissed()[i]);
+      }
+
+      //test totalCorrect
+      System.out.print("\nTotal Correct: ");
+      System.out.print(e.totalCorrect());
+
+      //test totalCorrect
+      System.out.print("\nTotal Incorrect: ");
+      System.out.print(e.totalIncorrect());
+   }
 }
 
